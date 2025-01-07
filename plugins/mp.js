efe36a3a7cf12d63c13a9fa689cdfe83ab0c1f4d
LAKSIDU_NIMSARA <laksidunimsara200803041@gmail.com>
2025-01-07 20:24:26 +0530

    Update mp.js

--- plugins/mp.js
+++ plugins/mp.js
@@ -1,9 +1,9 @@
 
 const {cmd , commands} = require('../command')
 const fg = require('api-dylux')
-const yts = require('yt-search')
+const yts = require('@dark-yasiya/yt-dl.js')
 cmd({
-    pattern: "song",
+    pattern: "ong",
     desc: "To download songs.",
     react: "🎵",
     category: "download",
