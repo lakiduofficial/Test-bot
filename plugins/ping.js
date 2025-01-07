b3b70f6dc08d3aebc9c56d39727cbe4d101480b8
LAKSIDU_NIMSARA <laksidunimsara200803041@gmail.com>
2025-01-07 15:07:40 +0530

    Update ping.js

--- plugins/ping.js
+++ plugins/ping.js
@@ -15,7 +15,7 @@ cmd({
 async(conn, mek, m,{from, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
 var inital = new Date().getTime();
-let ping = await conn.sendMessage(from , { text: '*QUEEN-ISHU-MD-SPEED*'  }, { quoted: mek } )
+let ping = await conn.sendMessage(from , { text: '*𝐐𝐔𝐄𝐄𝐍 𝐇𝐀𝐒𝐇𝐈 𝐌𝐃 𝐒𝐏𝐄𝐄𝐃*'  }, { quoted: mek } )
 var final = new Date().getTime();
 await conn.sendMessage(from, { delete: ping.key })
 return await conn.sendMessage(from , { text: '*👀Pong*\n *' + (final - inital) + ' ms* '  }, { quoted: mek } )
