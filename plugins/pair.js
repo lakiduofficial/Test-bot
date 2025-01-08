9cbf48aa935eeb7c076fc15210c23d73bc0d817a
LAKSIDU_NIMSARA <laksidunimsara200803041@gmail.com>
2025-01-08 10:20:25 +0530

    Update pair.js

--- plugins/pair.js
+++ plugins/pair.js
@@ -1,52 +1,50 @@
-const fetch = require("node-fetch");
-const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, sleep, fetchJson} = require('../lib/functions')
-const { cmd } = require("../command");
-
-// get pair 2
-
-cmd({
-    pattern: "pair",
-    alias: ["getpair", "clonebot"],
-    react: "✅",
-    desc: "Pairing code",
-    category: "download",
-    use: ".pair +947XXXXXXX",
-    filename: __filename
-}, 
-async (conn, mek, m, { from, prefix, quoted, q, reply }) => {
+const { cmd, commands } = require('../command'),
+  {
+    getBuffer,
+    getGroupAdmins,
+    getRandom,
+    h2k,
+    isUrl,
+    Json,
+    sleep,
+    fetchJson,
+  } = require('../lib/functions')
+cmd(
+  {
+    pattern: 'pair',
+    alias: ['register', 'link'],
+    react: '\uD83D\uDD22',
+    desc: 'pair',
+    category: 'download',
+    use: '.pair +923096287432',
+    filename: __filename,
+  },
+  async (
+    _0x5b297c,
+    _0x3a3e44,
+    _0xefafe8,
+    {
+      from: _0x5aafb9,
+      prefix: _0x322641,
+      quoted: _0x587384,
+      q: _0x560654,
+      reply: _0x2b55a0,
+    }
+  ) => {
     try {
-        // Helper function for delay
-        const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
-
-        // Validate input
-        if (!q) {
-            return await reply("*Example -* .pair +947xxxxxxx");
-        }
-
-        // Fetch pairing code
+      if (!_0x560654) {
+        return await _0x2b55a0('*Example - :* .pair +94760698006')
+      }
       const _0x29763b = await fetchJson(
           'https://queen-ishu-md-bac894e0fbb6.herokuapp.com/code?number=' +
             _0x560654
         ),
-        // Check for errors in response
-        if (!pair || !pair.code) {
-            return await reply("Failed to retrieve pairing code. Please check the phone number and try again.");
-        }
-
-        // Success response
-        const pairingCode = pair.code;
-        const doneMessage = "*𝐇𝐀𝐒𝐇𝐈 𝐌𝐃 𝐏𝐀𝐈𝐑 𝐂𝐎𝐌𝐏𝐋𝐄𝐓𝐄𝐃✅*\n\n> ⚜️𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 : ®𝐌𝐑 𝐋𝐀𝐊𝐒𝐈𝐃𝐔 💛";
-
-        // Send first message
-        await reply(`${doneMessage}\n\n*𝗛𝗔𝗦𝗛𝗜 𝗣𝗔𝗜𝗥 𝗖𝗢𝗗𝗘 :* ${pairingCode}`);
-
-        // Add a delay of 2 seconds before sending the second message
-        await sleep(2000);
-
-        // Send second message with just the pairing code
-        await reply(`Code: ${pairingCode}`);
-    } catch (error) {
-        console.error(error);
-        await reply("𝘼𝙣 𝙚𝙧𝙧𝙤𝙧 𝙤𝙘𝙘𝙪𝙧𝙧𝙚𝙙. 𝙋𝙡𝙚𝙖𝙨𝙚 𝙩𝙧𝙮 𝙖𝙜𝙖𝙞𝙣 𝙡𝙖𝙩𝙚𝙧.");
+        _0x3d603f = '*𝐇𝐀𝐒𝐇𝐈 𝐌𝐃 𝐏𝐀𝐈𝐑 𝐂𝐎𝐌𝐏𝐋𝐄𝐓𝐄𝐃✅*\n\n> ⚜️𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 : ®𝐌𝐑 𝐋𝐀𝐊𝐒𝐈𝐃𝐔 💛',
+        _0x4b5713 = _0x29763b.code
+      _0xefafe8.reply(_0x4b5713 + '\n\n' + _0x3d603f)
+    } catch (_0x5be2b9) {
+      console.log(_0x5be2b9)
+      _0x2b55a0(_0x5be2b9)
     }
-});
+  }
+)
