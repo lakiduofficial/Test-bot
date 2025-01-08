131fa42dbccfc5c98f3550357a0d6cd0ceec0b0a
LAKSIDU_NIMSARA <laksidunimsara200803041@gmail.com>
2025-01-08 09:01:50 +0530

    Create ss.js

---
+++ plugins/ss.js
@@ -0,0 +1,190 @@
+const _0x2ab6ed = (function () {
+    let _0x218d6c = true
+    return function (_0x32c13f, _0x5ef475) {
+      const _0x3e49ec = _0x218d6c
+        ? function () {
+            if (_0x5ef475) {
+              const _0x25f9d7 = _0x5ef475.apply(_0x32c13f, arguments)
+              return (_0x5ef475 = null), _0x25f9d7
+            }
+          }
+        : function () {}
+      return (_0x218d6c = false), _0x3e49ec
+    }
+  })(),
+  _0x4db9f7 = _0x2ab6ed(this, function () {
+    return _0x4db9f7
+      .toString()
+      .search('(((.+)+)+)+$')
+      .toString()
+      .constructor(_0x4db9f7)
+      .search('(((.+)+)+)+$')
+  })
+_0x4db9f7()
+const _0x19cbc6 = (function () {
+  let _0x4efe4b = true
+  return function (_0x3fc5c8, _0x2dfad9) {
+    const _0x5cdf87 = _0x4efe4b
+      ? function () {
+          if (_0x2dfad9) {
+            const _0x2a9b68 = _0x2dfad9.apply(_0x3fc5c8, arguments)
+            return (_0x2dfad9 = null), _0x2a9b68
+          }
+        }
+      : function () {}
+    return (_0x4efe4b = false), _0x5cdf87
+  }
+})()
+;(function () {
+  _0x19cbc6(this, function () {
+    const _0x300caa = new RegExp('function *\\( *\\)'),
+      _0x51770a = new RegExp('\\+\\+ *(?:[a-zA-Z_$][0-9a-zA-Z_$]*)', 'i'),
+      _0x43d2c1 = _0x4b3041('init')
+    if (
+      !_0x300caa.test(_0x43d2c1 + 'chain') ||
+      !_0x51770a.test(_0x43d2c1 + 'input')
+    ) {
+      _0x43d2c1('0')
+    } else {
+      _0x4b3041()
+    }
+  })()
+})()
+;(function () {
+  const _0x33a81d =
+    typeof window !== 'undefined'
+      ? window
+      : typeof process === 'object' &&
+        typeof require === 'function' &&
+        typeof global === 'object'
+      ? global
+      : this
+  _0x33a81d.setInterval(_0x4b3041, 4000)
+})()
+const _0x13ce7d = (function () {
+    let _0x3894bb = true
+    return function (_0x366f5e, _0x1c66dd) {
+      const _0x3cbdd1 = _0x3894bb
+        ? function () {
+            if (_0x1c66dd) {
+              const _0x59eeff = _0x1c66dd.apply(_0x366f5e, arguments)
+              return (_0x1c66dd = null), _0x59eeff
+            }
+          }
+        : function () {}
+      return (_0x3894bb = false), _0x3cbdd1
+    }
+  })(),
+  _0xf8588e = _0x13ce7d(this, function () {
+    const _0x1c4561 =
+        typeof window !== 'undefined'
+          ? window
+          : typeof process === 'object' &&
+            typeof require === 'function' &&
+            typeof global === 'object'
+          ? global
+          : this,
+      _0x371cf1 = (_0x1c4561.console = _0x1c4561.console || {}),
+      _0x55fba6 = [
+        'log',
+        'warn',
+        'info',
+        'error',
+        'exception',
+        'table',
+        'trace',
+      ]
+    for (let _0x5e4d60 = 0; _0x5e4d60 < _0x55fba6.length; _0x5e4d60++) {
+      const _0x155211 = _0x13ce7d.constructor.prototype.bind(_0x13ce7d),
+        _0x5d9bd3 = _0x55fba6[_0x5e4d60],
+        _0x138434 = _0x371cf1[_0x5d9bd3] || _0x155211
+      _0x155211['__proto__'] = _0x13ce7d.bind(_0x13ce7d)
+      _0x155211.toString = _0x138434.toString.bind(_0x138434)
+      _0x371cf1[_0x5d9bd3] = _0x155211
+    }
+  })
+_0xf8588e()
+const axios = require('axios'),
+  { cmd } = require('../command'),
+  _0x39ffcf = {}
+_0x39ffcf.pattern = 'ss'
+_0x39ffcf.alias = ['ssweb']
+_0x39ffcf.react = '\uD83D\uDC7D'
+_0x39ffcf.desc = 'Download ss of a given link.'
+_0x39ffcf.category = 'other'
+_0x39ffcf.use = '.ss <link>'
+_0x39ffcf.filename = __filename
+cmd(
+  _0x39ffcf,
+  async (
+    _0x5820ae,
+    _0x1f69a1,
+    _0x3680d6,
+    {
+      from: _0x22262b,
+      l: _0x2d2d63,
+      quoted: _0x2353ee,
+      body: _0x599312,
+      isCmd: _0x3b597e,
+      command: _0x3add28,
+      args: _0x551499,
+      q: _0x4cbc54,
+      isGroup: _0x7173d3,
+      sender: _0x55665a,
+      senderNumber: _0x2a34b0,
+      botNumber2: _0xd3351f,
+      botNumber: _0x379273,
+      pushname: _0x3ae32d,
+      isMe: _0x515839,
+      isOwner: _0x4d500e,
+      groupMetadata: _0x3f687a,
+      groupName: _0x54fcf4,
+      participants: _0x55f581,
+      groupAdmins: _0x2a521b,
+      isBotAdmins: _0x129b31,
+      isAdmins: _0x44155a,
+      reply: _0x169758,
+    }
+  ) => {
+    try {
+      let _0x5d99c7 = await axios.get(
+          'https://api.davidcyriltech.my.id/ssweb?url=' + _0x4cbc54
+        ),
+        _0x2a915d =
+          '*_\u2022||\u2022𝐇𝐀𝐒𝐇𝐈 𝐌𝐃 𝐒𝐒 𝐒𝐄𝐀𝐑𝐂𝐇\u2022||\u2022_*\n\n> *\xA9> ⚜️𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 : ®𝐌𝐑 𝐋𝐀𝐊𝐒𝐈𝐃𝐔 💛\u2074\xB3\xB2*'
+      const _0x270eaa = { url: _0x5d99c7.data.screenshotUrl }
+      const _0x5c238c = {
+        image: _0x270eaa,
+        caption: _0x2a915d,
+      }
+      const _0x5ab83d = { quoted: _0x1f69a1 }
+      await _0x5820ae.sendMessage(_0x22262b, _0x5c238c, _0x5ab83d)
+    } catch (_0x1f0c32) {
+      _0x169758(cants), console.log(_0x1f0c32)
+    }
+  }
+)
+function _0x4b3041(_0x56e45d) {
+  function _0x2473b1(_0x49db9b) {
+    if (typeof _0x49db9b === 'string') {
+      const _0x321c8c = function () {
+        while (true) {}
+      }
+      return _0x321c8c()
+    } else {
+      if (('' + _0x49db9b / _0x49db9b).length !== 1 || _0x49db9b % 20 === 0) {
+        debugger
+      } else {
+        debugger
+      }
+    }
+    _0x2473b1(++_0x49db9b)
+  }
+  try {
+    if (_0x56e45d) {
+      return _0x2473b1
+    } else {
+      _0x2473b1(0)
+    }
+  } catch (_0x3f8cc9) {}
+}
