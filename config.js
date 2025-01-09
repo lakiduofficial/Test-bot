e34a4e4e0e6585a27f43d09b31e607732dd3647e
lakiduofficial <dilamod35@gmail.com>
2025-01-09 05:53:45 +0530

    Update config.js

--- config.js
+++ config.js
@@ -19,7 +19,7 @@ PASTEBIN_API_KEY: process.env.PASTEBIN_API_KEY || "uh8QvO6vQJGtIug9WvjdTAPx_ZAFJ
 OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39",
 ///=================ඕවා එඩිට් කරද්දි කිසිම දෙයක් වැරදියට දමන්න එපා !!ALIVE-CONFIG===============================
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.imgur.com/vzDwkjg.jpeg", //add alive img url
-ALIVE_MSG: process.env.ALIVE_MSG || "💜✅ HI I AM WHATSAPP BOT ✅💜", //add alive msg
+ALIVE_MSG: process.env.ALIVE_MSG || "💜✅ HI I AM  BOT ✅💜", //add alive msg
 //=========================මේ උඩ තියන දෙක වෙනස් කරන්න එපා වෙනස් කරත් වැඩක් නෑ=============
 ANTI_LINK: process.env.ANTI_LINK || "true", //add true or false
 ANTI_BAD: process.env.ANTI_BAD || "true", //add true or false
